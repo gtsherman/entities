edcb3b33009269cf3d1ab9bf2278613092120bad
Garrick Sherman <gsherma2@illinois.edu>
2016-03-04 15:16:28 -0600

    added DELM code (Tao et al.)

---
+++ src/main/java/edu/gslis/main/RunDELMRetrieval.java
@@ -0,0 +1,94 @@
+package edu.gslis.main;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Iterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.gslis.delm.DocumentExpander;
+import edu.gslis.docscoring.support.CollectionStats;
+import edu.gslis.docscoring.support.IndexBackedCollectionStats;
+import edu.gslis.entities.docscoring.ScorerDirichlet;
+import edu.gslis.indexes.IndexWrapperIndriImpl;
+import edu.gslis.output.FormattedOutputTrecEval;
+import edu.gslis.queries.GQueriesJsonImpl;
+import edu.gslis.queries.GQuery;
+import edu.gslis.searchhits.SearchHit;
+import edu.gslis.searchhits.SearchHits;
+import edu.gslis.textrepresentation.FeatureVector;
+import edu.gslis.utils.Configuration;
+import edu.gslis.utils.SimpleConfiguration;
+import edu.gslis.utils.Stopper;
+
+public class RunDELMRetrieval {
+	
+	static final Logger logger = LoggerFactory.getLogger(RunDELMRetrieval.class);
+	
+	public static void main(String[] args) {
+		Configuration config = new SimpleConfiguration();
+		config.read(args[0]);
+		
+		IndexWrapperIndriImpl index = new IndexWrapperIndriImpl(config.get("index"));
+		
+		Stopper stopper = null;
+		if (config.get("stoplist") != null)
+			stopper = new Stopper(config.get("stoplist"));
+		
+		GQueriesJsonImpl queries = new GQueriesJsonImpl();
+		queries.read(config.get("queries"));
+		
+		CollectionStats cs = new IndexBackedCollectionStats();
+		cs.setStatSource(config.get("index"));
+		
+		int numDocs = 3000;
+		if (config.get("num-docs") != null) {
+			numDocs = Integer.parseInt(config.get("num-docs"));
+		}
+		
+		double alpha = 0.5;
+		if (config.get("alpha") != null) {
+			alpha = Double.parseDouble(config.get("alpha"));
+		}
+
+		ScorerDirichlet scorer = new ScorerDirichlet();
+		scorer.setCollectionStats(cs);
+		scorer.setParameter(scorer.PARAMETER_NAME, 2500);
+
+		Writer outputWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		FormattedOutputTrecEval output = FormattedOutputTrecEval.getInstance("categories", outputWriter);
+		
+		Iterator<GQuery> queryIt = queries.iterator();
+		int i = 0;
+		while (queryIt.hasNext()) {
+			GQuery query = queryIt.next();
+
+			i++;
+			logger.info("Working on query "+query.getTitle()+". ("+i+"/"+queries.numQueries()+")");
+
+			if (stopper != null)
+				query.applyStopper(stopper);
+			scorer.setQuery(query);
+			
+			SearchHits hits = index.runQuery(query, numDocs);
+			Iterator<SearchHit> hitIt = hits.iterator();
+			while (hitIt.hasNext()) {
+				SearchHit hit = hitIt.next();
+				FeatureVector dv = index.getDocVector(hit.getDocID(), null);
+				hit.setFeatureVector(dv);
+				hit.setLength(dv.getLength());
+				
+				DocumentExpander expander = new DocumentExpander(index, stopper);
+				expander.expand(hit, alpha);
+				
+				hit.setFeatureVector(expander.getPseudoDocVector());
+				hit.setScore(scorer.score(hit));
+			}
+			hits.rank();
+			output.write(hits, query.getTitle());
+		}
+	}
+
+}
---
+++ src/main/java/edu/gslis/similarity/CosineSimilarityScorer.java
@@ -0,0 +1,66 @@
+package edu.gslis.similarity;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.gslis.textrepresentation.FeatureVector;
+
+public class CosineSimilarityScorer {
+	
+	private static Logger logger = LoggerFactory.getLogger(CosineSimilarityScorer.class);
+	
+	private FeatureVector doc;
+	private FeatureVector otherdoc;
+	
+	public CosineSimilarityScorer(FeatureVector doc, FeatureVector otherdoc) {
+		setDocs(doc, otherdoc);
+	}
+	
+	public void setDocs(FeatureVector doc, FeatureVector otherdoc) {
+		this.doc = doc;
+		this.otherdoc = otherdoc;
+	}
+	
+	public FeatureVector[] getDocs() {
+		FeatureVector[] docs = {this.doc, this.otherdoc};
+		return docs;
+	}
+	
+	public double score() {
+		/*double num = 0.000001;
+		double denomX = 0.000001;
+		double denomY = 0.000001;
+		*/
+		double num = 0.0;
+		double denomX = 0.0;
+		double denomY = 0.0;
+		
+		Set<String> vocab = new HashSet<String>();
+		vocab.addAll(doc.getFeatures());
+		vocab.addAll(otherdoc.getFeatures());
+
+		logger.debug("Doc length 1: "+doc.getFeatures().size());
+		logger.debug("Doc length 2:" +otherdoc.getFeatures().size());
+		logger.debug("Total: "+vocab.size());
+		
+		Iterator<String> termIt = vocab.iterator();
+		String term;
+		while (termIt.hasNext()) {
+			term = termIt.next();
+			
+			double docWeight = doc.getFeatureWeight(term);
+			double otherdocWeight = otherdoc.getFeatureWeight(term); 
+			
+			num += docWeight * otherdocWeight;
+			denomX += Math.pow(docWeight, 2);
+			denomY += Math.pow(otherdocWeight, 2);
+		}
+		double denom = Math.sqrt(denomX)*Math.sqrt(denomY);
+		if (denom == 0) denom = 1;  // should be unnecessary...
+		return num/denom;
+	}
+}
