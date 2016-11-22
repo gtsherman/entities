d811db6195576ef498fd9ca9996cca350d57ec94
Garrick Sherman <gsherma2@illinois.edu>
2016-11-22 10:42:58 -0800

    added FileLookupDocScorerCreator

---
+++ src/main/java/edu/gslis/entities/docscoring/creators/FileLookupDocScorerCreator.java
@@ -0,0 +1,39 @@
+package edu.gslis.entities.docscoring.creators;
+
+import java.io.File;
+
+import edu.gslis.entities.docscoring.DocScorer;
+import edu.gslis.entities.docscoring.FileLookupDocScorer;
+import edu.gslis.entities.docscoring.StoredDocScorer;
+import edu.gslis.searchhits.SearchHit;
+
+/**
+ * DocScorerCreator for FileLookupDocScorer.
+ * Note: It is very important to set the SearchHit's query name property if you
+ * need that to be included in the file lookup path.
+ * 
+ * @author Garrick
+ *
+ */
+public class FileLookupDocScorerCreator extends DocScorerCreator {
+	
+	private String basePath;
+	
+	public FileLookupDocScorerCreator(String basePath) {
+		this.basePath = basePath;
+	}
+
+	@Override
+	protected void createIfNecessary(SearchHit doc) {
+		if (doc.getQueryName() == null) {
+			doc.setQueryName("");
+		}
+
+		if (!storedScorers.containsKey(doc.getQueryName() + File.separator + doc.getDocno())) {
+			DocScorer docScorer = new FileLookupDocScorer(basePath + File.separator +
+					doc.getQueryName() + File.separator + doc.getDocno());
+			storedScorers.put(doc.getDocno(), new StoredDocScorer(docScorer));
+		}
+	}
+
+}
